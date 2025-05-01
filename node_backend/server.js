2383ed7073ee856b8ea186602e2b640e6363ae7e
Raja Ahmed Mustafa Taimur <rb4733164@gmail.com>
2025-05-01 22:37:15 +0500

    Updated database connection to use environment variables

--- node_backend/server.js
+++ node_backend/server.js
@@ -41,7 +41,7 @@ const db = mysql.createConnection({
   host: process.env.MYSQL_HOST || 'mysql.railway.internal',
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'wPchUnlzWGmWGJZdUJCwhIWfNYYBYPMi',
-  database: process.env.MYSQL_DATABASE || 'railway'
+  database: process.env.MYSQL_DATABASE || 'railway',
 });
 
 
