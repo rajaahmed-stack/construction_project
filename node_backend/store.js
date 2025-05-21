6dce4748ff317d6050cb12999269a371aba5754d
Raja Ahmed Mustafa Taimur <rb4733164@gmail.com>
2025-05-21 12:56:19 +0500

    Updated database connection to use environment variables

--- node_backend/store.js
+++ node_backend/store.js
@@ -120,9 +120,12 @@ UNION ALL
 
 SELECT 
     i.work_order_id, 
+    NULL AS permission_number,
     eam.job_type,
     eam.sub_section,
-    eam.file_path
+    eam.file_path,
+    NULL AS survey_file_path,
+    NULL AS Document
 FROM invoice i
 JOIN emergency_and_maintainence eam 
     ON i.work_order_id = eam.work_order_id
@@ -130,6 +133,7 @@ JOIN work_receiving wr
     ON eam.work_order_id = wr.work_order_id
 WHERE i.work_order_id NOT IN (SELECT work_order_id FROM store)
   AND wr.current_department = 'Store';
+
 
 
       `;
